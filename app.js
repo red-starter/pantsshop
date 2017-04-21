7781208e3f2c73746ad52e0c7dff7c4a4c288f30
Michael Sova <michael.sova@datafox.com>
2017-04-20 20:18:16 -0400

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-// app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
